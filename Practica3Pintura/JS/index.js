ff0e2ad08e6ec6d1b9137fcf132919aa8483f74d
art321995 <art270112@gmail.com>
2021-03-22 14:22:52 -0600

    Creacion de Costo de Pintura

---
+++ Practica3Pintura/JS/index.js
@@ -0,0 +1,14 @@
+let btnCalcular = document.getElementById("btnCalcular");
+
+btnCalcular.addEventListener('click', calcularCosto);
+
+function calcularCosto() {
+    let costoMateriales = Number( document.getElementById("costoMaterial").value);
+    let anchoPared = Number(document.getElementById("AnchoPared").value);
+    let altoPared = Number(document.getElementById("AltoPared").value);
+
+    let resultado = altoPared * anchoPared * costoMateriales;
+
+    let containerResultado = document.getElementById("resultado");
+   
+}
