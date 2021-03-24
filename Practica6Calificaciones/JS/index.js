e1a23822e351bd5379854434364eb594e785f53d
art321995 <art270112@gmail.com>
2021-03-24 13:03:08 -0600

    Calificaciones Correctas

--- Practica6Calificaciones/JS/index.js
+++ Practica6Calificaciones/JS/index.js
@@ -3,15 +3,16 @@ let btnCalcular = document.getElementById("btnCalcularPromedio");
 btnCalcular.addEventListener("click", CalcularPromedio);
 
 function CalcularPromedio(){
-    let calificacion1 = document.getElementById("cali1").value;
-    let calificacion2 = document.getElementById("cali2").value;
-    let calificacion3 = document.getElementById("cali3").value;
-    let calificacion4 = document.getElementById("cali4").value;
-    let calificacion5 = document.getElementById("cali5").value;
+    let calificacion1 = Number (document.getElementById("cali1").value);
+    let calificacion2 = Number (document.getElementById("cali2").value);
+    let calificacion3 = Number (document.getElementById("cali3").value);
+    let calificacion4 = Number (document.getElementById("cali4").value);
+    let calificacion5 = Number (document.getElementById("cali5").value);
 
+    let Matricula = document.getElementById("matricula").value;
     let resultado = (calificacion1 + calificacion2 + calificacion3 + calificacion4 + calificacion5 ) / 5;
 
     let containerResultado = document.getElementById("resultado");
 
-    containerResultado.innerText = 'El proemedio es:'  + resultado;
+    containerResultado.innerText = 'El proemedio es '+ resultado + ' Con matricula '+ Matricula;
 } 
