3c4795b8e5d86a12757f11af0cf72c959de0349a
art321995 <art270112@gmail.com>
2021-04-07 19:33:20 -0500

    Tres condiciones Anidadas

---
+++ Practica23TresCondicionesAnidadas/JS/index.js
@@ -0,0 +1,21 @@
+const btnCalcularPromedio = document.getElementById("btnCalcular");
+
+btnCalcularPromedio.addEventListener("click",calcularPromedios);
+
+function calcularPromedios(){
+
+    const Promedio = Number(document.getElementById("promedio").value);
+     let resultado = "";
+
+
+     if(Promedio >= 9){
+        resultado = `<div class = "alert alert-success"> Aprobado</div>`;
+     } else if(Promedio >= 6 && Promedio <9){
+        resultado = "Regular";
+     } else if(Promedio < 6){
+        resultado = "Reprobado";
+     }
+
+    const containerResult = document.getElementById("containerResult");
+    containerResult.innerHTML = resultado;
+}
