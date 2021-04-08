3e7c32493c1d69632f8dc538e148b399e536815c
art321995 <art270112@gmail.com>
2021-04-08 18:48:35 -0500

    Langossta ahumada

--- Practica24LangostaAhumada/JS/index.js
+++ Practica24LangostaAhumada/JS/index.js
@@ -3,20 +3,22 @@ const btnCalcularPrecio = document.getElementById("btnCalcular");
 btnCalcularPrecio.addEventListener("click",calcularPrecios);
 
 function calcularPrecios(){
-    const Precio = Number(document.getElementById("precio").value);
+    const Precio = Number(document.getElementById("personas").value);
      let resultado = "";
+      let total = "";
 
      if(Precio < 200){
-        resultado = `<div class = "alert alert-success"> Aprobado </div>`;
-     } else if(Numero1 >= 200 && Numero1 <300){
-        resultado = "El numero cuenta 2 digitos ";
-     } else if(Numero1 > 0 && Numero1 <= 9){
-        resultado = "El numero cuenta 1 digito ";
-     } else{
-        resultado = "El numero cuenta con mas de 3 digitos ";
+        total= Precio * 95;
+        resultado = `<div class = "alert alert-danger"> El platillo por persona te sale en $95.00  +" El total es $"+ total+".00" </div>`;
+     } else if(Precio >= 200 && Precio <300){
+      total= Precio * 85;
+        resultado = `<div class = "alert alert-warning"> El platillo por persona te sale en $85.00 </div>` +" El total es $"+ total+".00";
+     } else if(Precio >=300){
+      total= Precio * 75;
+        resultado = `<div class = "alert alert-success"> El platillo por persona te sale en $75.00 </div>` +" El total es $"+ total+".00";
      }
 
     const containerResult = document.getElementById("containerResult");
-    containerResult.innerText = resultado;
+    containerResult.innerHTML = resultado;
 
 }
