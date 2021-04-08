53e1c55323a912529713c7a302fffaa1b009a929
art321995 <art270112@gmail.com>
2021-04-07 19:33:00 -0500

    .

--- Practica22ElseIf/JS/index.js
+++ Practica22ElseIf/JS/index.js
@@ -25,3 +25,23 @@ function calcularDigitos(){
 
     containerResult.innerText = resultado;
 }
+
+function CalcularDigitosOL(){
+
+    const Numero1 = Number(document.getElementById("numero").value);
+     let resultado = "";
+
+
+     if(Numero1 > 99 && Numero1 <= 999 ){
+        resultado = "El numero cuenta 3 digitos ";
+     } else if(Numero1 > 9 && Numero1 <=99){
+        resultado = "El numero cuenta 2 digitos ";
+     } else if(Numero1 > 0 && Numero1 <= 9){
+        resultado = "El numero cuenta 1 digito ";
+     } else{
+        resultado = "El numero cuenta con mas de 3 digitos ";
+     }
+
+    const containerResult = document.getElementById("containerResult");
+    containerResult.innerText = resultado;
+}
