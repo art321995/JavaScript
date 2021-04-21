a680ce8c90246321846b595e12444c483cfc40e3
art321995 <art270112@gmail.com>
2021-04-21 18:33:54 -0500

    Impuestos de agencia de Carros.

---
+++ Practica61AgenciaCarros/JS/index.js
@@ -0,0 +1,48 @@
+const btnInitApp = document.getElementById("btnInitApp");
+
+btnInitApp.addEventListener("click",evaluarImpuesto);
+
+function evaluarImpuesto(){
+
+    let numautos = prompt("Ingrese con cuantos autos cuenta");
+    let impuesto = "";
+    let precio = "";
+    let total = "";
+    let clave ="";
+    let clave1 = "";
+    let clave2 = "";
+    let clave3 = "";
+    
+
+
+    for(let i = 1; i <= numautos; i++){
+        precio =  Number (prompt( `Ingrese el $precio$ del auto ${i}:`));
+        clave = prompt(`Ingrese la calve del auto ${i}: `);
+        if(clave == 1){
+            impuesto = precio * .10;
+            clave1 = clave1 + impuesto;
+        }else if(clave == 2){
+            impuesto = precio * .07;
+            clave2 = clave2 + impuesto;
+        }else if(clave == 3){
+            impuesto = precio *.05;
+            clave3 = clave3 + impuesto;
+        }else {
+            impuesto = alert("Solo puedes ingresar numeros del 1 al 3");
+            return;
+        }
+        total = total + impuesto;
+
+        let resulContainer = document.getElementById("resultContainer");
+        resulContainer.innerHTML=(`El auto numero ${i} paga de impuestos $${impuesto.toFixed(2)}`)
+
+       
+    }
+
+    let resulContainer1 = document.getElementById("resultContainer1");
+    resulContainer1.innerHTML=(`El total a pagar de impuesto de la categoria 1 es: $${clave1}<br>
+                               El total a pagar de impuesto de la categoria 2 es: $${clave2}<br>
+                               El total a pagar de impuesto de la categoria 3 es: $${clave3}<br>
+                               El total a pagar de impuestos por todos los vehiculos es: $${total} `)
+    
+}
