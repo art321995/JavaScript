d7430121986574e026abae2fb5a0c8bd2fe16253
art321995 <art270112@gmail.com>
2021-04-22 18:52:20 -0500

    ....

--- Practica47Paresen100/JS/index.js
+++ Practica47Paresen100/JS/index.js
@@ -4,15 +4,16 @@ btnInitApp.addEventListener("click",imprimirNumeros);
 
 function imprimirNumeros(){
     let resultadoAcumulado = "";
+    let suma= 0;
 
     for(let i=0;i<=100;i++){
         if(i % 2 == 0){
             resultadoAcumulado += `<li class="list-group-item"> EL numero ${i} es par </li>`
         }
-
+        suma = suma + resultadoAcumulado;
     }
 
     let resulContainer = document.getElementById("resultContainer");
 
-    resulContainer.innerHTML = resultadoAcumulado;
+    resulContainer.innerHTML = suma;
 }
