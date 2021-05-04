d7b535c27740c9e6db00e973ed13c9248ec759b3
art321995 <art270112@gmail.com>
2021-05-03 19:44:23 -0500

    Array de Numeros Primos hasta el 1000

---
+++ Practica77ArrayNumerosPrimos/JS/index.js
@@ -0,0 +1,44 @@
+const btnMostrarValores = document.getElementById("btnMostarValores");
+
+btnMostrarValores.addEventListener("click", mostrarValores);
+
+let NumPrimo = [];
+
+function AlmacenarNumerosPrimos(){
+
+    for(let index = 0; index<=1000; index++){
+        let numero = Math.floor(Math.random()*(100 - 1 + 1) + 1);
+        let isPrimo = ValidarNumeroPrimo(numero);
+
+        if(isPrimo == true){
+            NumPrimo[index] = numero;
+        }else {
+            index--;
+        }
+    }
+}
+
+function ValidarNumeroPrimo(numero){
+    for(let divisor = 2; divisor < numero; divisor++)
+{
+    if(numero % divisor == 0){
+    return false;
+    }
+}
+return true;
+}
+
+function mostrarValores(){
+    const lstValoresAlmacenados = document.getElementById("contenedorResultado");
+
+    let mensajeResultado = "";
+
+    for(let indice = 0; indice < NumPrimo.length; indice++){
+        mensajeResultado += `<li class="list-group-item">Indice ${indice} Numero Primo ${NumPrimo[indice]}</li>`;
+    }
+
+    lstValoresAlmacenados.innerHTML = mensajeResultado;
+
+    NumPrimo =[];
+    index = 0;
+}
