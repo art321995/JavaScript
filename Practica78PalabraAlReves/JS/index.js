6b29ec53d59bb12babae7380fdae2c4587b43d3e
art321995 <art270112@gmail.com>
2021-05-13 10:36:42 -0500

    Practica Palabra al Reves

---
+++ Practica78PalabraAlReves/JS/index.js
@@ -0,0 +1,31 @@
+const btnApp = document.getElementById("btnMostarValores");
+
+btnApp.addEventListener("click", mostrarPalabra);
+
+let arrayPalabra = [];
+let arrayPalabraReves = [];
+
+function almacenarArray(){
+
+    let arrayPalabra = prompt("Ingrese solo una palabra");
+        
+        if(isNaN(arrayPalabra) == false && arrayPalabra != null){
+            alert("Ingrese una palabra");
+        }else if(/\s/.test(arrayPalabra)){
+            alert("Ingrese una sola palabra");
+        }else if(arrayPalabra == null){
+            return;
+        }else{
+            let indiceReves = arrayPalabra.length-1;
+            for(let indice = 0; indice<arrayPalabra.length; indice++){
+                arrayPalabraReves[indiceReves] = arrayPalabra[indice];
+                indiceReves--;
+            }
+        }
+        mostrarPalabra();
+}
+
+function mostrarPalabra(){
+    let resultContainer = "";
+    resultContainer.innerHTML = palabraReves;
+}
